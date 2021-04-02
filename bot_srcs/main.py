4518f3785b7de18d99d2b6dc3757fefcd459f9c6
Lena <helly.luchina@gmail.com>
2021-04-02 19:37:35 +0300

    init start, help commands and hi bye responses

---
+++ bot_srcs/main.py
@@ -0,0 +1,27 @@
+import Constants as keys
+from telegram.ext import *
+import Commands as cmd
+import Responses as resp
+
+print("Bot started...")
+
+def handle_message(update, context):
+	response = resp.letsbot_responses(update.message.text)
+	update.message.reply_text(response)
+
+def error(update, context):
+	print(f"Update {update} caused error {context.error}")
+
+def main():
+	updater = Updater(keys.API_KEY, use_context=True)
+	dp = updater.dispatcher
+
+	dp.add_handler(CommandHandler("start", cmd.start_command))
+	dp.add_handler(CommandHandler("help", cmd.help_command))
+	dp.add_handler(MessageHandler(Filters.text, handle_message))
+	dp.add_error_handler(error)
+
+	updater.start_polling()
+	updater.idle()
+
+main()
