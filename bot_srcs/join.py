418a673e4c317e83cad7b6482acb196f233cc275
Lena <helly.luchina@gmail.com>
2021-05-05 20:39:51 +0300

    loop in id query

--- bot_srcs/join.py
+++ bot_srcs/join.py
@@ -2,6 +2,9 @@ from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, Conve
 from database.Requests import *
 
 def start_conv(update, context):
+	# for test
+	# mid = add_meeting("koko", update.message.chat.id)
+	# update.message.reply_text(str(mid))
 	update.message.reply_text('Чтобы стать участником мероприятия, тебе необходим идентификатор. Попроси его у организатора и отправь мне, я сделаю всю остальную работу :)\nЕсли не хочешь никуда добавляться вызови /cancel')
 	return 1
 
@@ -18,7 +21,7 @@ def get_id(update, context):
 	user_id =  update.message.chat.id
 	if (add_participant(meeting_id, user_id) == -1):
 		update.message.reply_text('Похоже, у тебя неверный идентификатор встречи.')
-		return ConversationHandler.END
+		return 1
 
 	update.message.reply_text('Тебя добавили в участники! Теперь ты сможешь поучаствовать в принятии решений и точно не пропустишь всю важную информацию. Чтобы получить сведения о встрече воспользуйся командой /get_meeting_info.')
 	return ConversationHandler.END
