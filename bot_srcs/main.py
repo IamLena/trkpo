bb05a4c799c6ac71613f02cb5639597a7b5fb314
Lena <helly.luchina@gmail.com>
2021-05-06 16:28:57 +0300

    initialization db in main

--- bot_srcs/main.py
+++ bot_srcs/main.py
@@ -1,11 +1,12 @@
 from telegram.ext import *
 from database.Requests import *
 from get_meeting_info import get_meeting_info_handler
+from database.CreateDB import *
 import Commands as cmd
 import Responses as resp
 import os
 
-create_tables()
+initialization("database/lets.db")
 
 def handle_message(update, context):
 	response = resp.letsbot_responses(update.message.text)
